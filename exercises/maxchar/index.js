c71ab4359d28c2a19741f1fb454e8b97c252dcd5
Ella Mae A <ellamae207@gmail.com>
2018-02-18 19:47:08 -1000

    passed maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,53 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+/*function maxChar(str) {
 
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for(let char of str){
+    //for every char in string
+    if(charMap[char]) {
+      //if there's a character then iterate by 1
+      charMap[char]++;
+    }else{
+      //otherwise set to one
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap){
+    //used to iterate thru an object - IN IN IN IN
+    if(charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
+*/
+
+function maxChar (str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str){
+    if(charMap[char]) {
+      charMap[char]++
+    }else{
+      charMap[char] = 1;
+    }
+  }
+  for (let char in charMap){
+    if (charMap[char] > max){
+      max = charMap[char]
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 module.exports = maxChar;
+
