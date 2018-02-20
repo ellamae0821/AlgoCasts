2cb1404688e12d6236b904429e10177a2dad0569
Ella Mae A <ellamae207@gmail.com>
2018-02-19 15:14:08 -1000

    passed pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,23 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const midpoint = Math.floor((2 *  n - 1) / 2);
+  //number of hashed column is twice the row minus one and tfind the midpoint by rounding it up.
+
+  for(let row = 0; row < n; row++) {
+    let level = '';
+
+    for(let column = 0; column < 2 * n -1; column ++){
+      if(midpoint - row <= column && midpoint + row >= column){
+        level += '#';
+
+      } else {
+      level += ' ';
+      }
+    }
+    console.log(level)
+  }
+}
 
 module.exports = pyramid;
