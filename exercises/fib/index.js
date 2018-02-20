fb7159ea1b95be8c36f901efa2205d899548ce16
Ella Mae A <ellamae207@gmail.com>
2018-02-19 16:14:50 -1000

    passes fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,18 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  const result = [0, 1];
+//trick: first 2 numbers can't be generated through a forr loop
+  for (let i=2; i<=n; i++){
+    const a = result[i - 1];
+    // first loop: i = 2 , so a is the 2nd element as 1
+    const b = result[i - 2];
+    //first loop: b is 2 - 2 = 0, so b's the 1st element which is 0
+
+    result.push(a + b);
+  }
+  return result[n];
+}
 
 module.exports = fib;
