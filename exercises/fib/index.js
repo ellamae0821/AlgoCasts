e2bde8418dc7913aaf635f15f8b1015fed4730ac
Ella Mae A <ellamae207@gmail.com>
2018-02-19 16:48:33 -1000

    passes memoization of fibo

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -7,7 +7,7 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
-
+/*
 function fib(n) {
   const result = [0, 1];
 //trick: first 2 numbers can't be generated through a forr loop
@@ -23,7 +23,7 @@ function fib(n) {
 }
 
 module.exports = fib;
-
+*/
 //store the arguments of each function call along with th result. If the function os called again with the same arguments, return the precomputed result, rather than running the function again.
 
 /*RECURSIVE
@@ -36,3 +36,32 @@ function fib(n){
   return fib(n - 1) + fib(n - 2);
 }
 */
+
+
+function memoize(fn) {
+  const cache = {};
+  //cache as our storage area,
+  return function (...args) {
+  // ...args because we are unsure of how many arguments will this function be called with
+    if(cache[args]){
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+//
+
+function slowFib(n){
+  if(n < 2){
+    return n;
+  }
+  return slowFib(n - 1) + slowFib(n - 2);
+}
+
+const fib = memoize(slowFib);
+
+module.exports = fib;
